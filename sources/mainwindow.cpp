f15e85d9efee6f3dc274129a0a2c1c72cf9dbc67
Asphox <aspirine026@gmail.com>
2018-05-15 02:39:11 +0200

    ajout de l'archi du projet et des fichiers de base Qt

---
+++ sources/mainwindow.cpp
@@ -0,0 +1,15 @@
+#include "../headers/mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowTitle(tr("Asserv'INT"));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
